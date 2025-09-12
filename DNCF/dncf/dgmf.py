014c727cbb0911da5557ec38aa3e02f724f0d555
jayarnim <jayarnim727@gmail.com>
2025-09-12 21:34:53 +0900

    commit

---
+++ DNCF/dncf/dgmf.py
@@ -0,0 +1,132 @@
+import torch
+import torch.nn as nn
+
+
+class Module(nn.Module):
+    def __init__(
+        self,
+        n_users: int,
+        n_items: int,
+        n_factors: int,
+        interactions: torch.Tensor, 
+    ):
+        super(Module, self).__init__()
+        # attr dictionary for load
+        self.init_args = locals().copy()
+        del self.init_args["self"]
+        del self.init_args["__class__"]
+
+        # device setting
+        DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
+        self.device = torch.device(DEVICE)
+
+        # global attr
+        self.n_users = n_users
+        self.n_items = n_items
+        self.n_factors = n_factors
+        self.interactions = interactions.to(self.device)
+
+        # generate layers
+        self._init_layers()
+
+    def forward(
+        self, 
+        user_idx: torch.Tensor, 
+        item_idx: torch.Tensor,
+    ):
+        """
+        user_idx: (B,)
+        item_idx: (B,)
+        """
+        return self.score(user_idx, item_idx)
+
+    def predict(
+        self, 
+        user_idx: torch.Tensor, 
+        item_idx: torch.Tensor,
+    ):
+        """
+        user_idx: (B,)
+        item_idx: (B,)
+        """
+        with torch.no_grad():
+            logit = self.score(user_idx, item_idx)
+            pred = torch.sigmoid(logit)
+        return pred
+
+    def score(self, user_idx, item_idx):
+        pred_vector = self.gmf(user_idx, item_idx)
+        logit = self.logit_layer(pred_vector).squeeze(-1)
+        return logit
+
+    def gmf(self, user_idx, item_idx):
+        rep_user, rep_item = self.rep(user_idx, item_idx)
+        pred_vector = rep_user * rep_item
+        return pred_vector
+
+    def rep(self, user_idx, item_idx):
+        user_id = self.user_id_embed(user_idx)
+        user_hist = self.user_hist_embed(user_idx, item_idx)
+        rep_user = user_id + user_hist
+
+        item_id = self.item_id_embed(item_idx)
+        item_hist = self.item_hist_embed(user_idx, item_idx)
+        rep_item = item_id + item_hist
+
+        return rep_user, rep_item
+
+    def user_hist_embed(self, user_idx, item_idx):
+        # get user vector from interactions
+        user_slice = self.interactions[user_idx, :-1].clone()
+        
+        # masking target items
+        user_batch = torch.arange(user_idx.size(0))
+        user_slice[user_batch, item_idx] = 0
+        
+        # projection
+        user_sum = user_slice.sum(dim=1, keepdim=True)
+        user_sum = torch.clamp(user_sum, min=1.0)
+        proj_user = self.proj_u(user_slice.float()) / torch.sqrt(user_sum)
+
+        return proj_user
+
+    def item_hist_embed(self, user_idx, item_idx):
+        # get item vector from interactions
+        item_slice = self.interactions.T[item_idx, :-1].clone()
+        
+        # masking target users
+        item_batch = torch.arange(item_idx.size(0))
+        item_slice[item_batch, user_idx] = 0
+        
+        # projection
+        item_sum = item_slice.sum(dim=1, keepdim=True)
+        item_sum = torch.clamp(item_sum, min=1.0)
+        proj_item = self.proj_i(item_slice.float()) / torch.sqrt(item_sum)
+
+        return proj_item
+
+    def _init_layers(self):
+        self.user_id_embed = nn.Embedding(
+            num_embeddings=self.n_users+1, 
+            embedding_dim=self.n_factors,
+            padding_idx=self.n_users,
+        )
+        self.item_id_embed = nn.Embedding(
+            num_embeddings=self.n_items+1, 
+            embedding_dim=self.n_factors,
+            padding_idx=self.n_items,
+        )
+        self.proj_u = nn.Linear(
+            in_features=self.n_items,
+            out_features=self.n_factors,
+            bias=False,
+        )
+        self.proj_i = nn.Linear(
+            in_features=self.n_users,
+            out_features=self.n_factors,
+            bias=False,
+        )
+        self.logit_layer = nn.Linear(
+            in_features=self.n_factors,
+            out_features=1,
+        )
---
+++ DNCF/dncf/dmlp.py
@@ -0,0 +1,165 @@
+import torch
+import torch.nn as nn
+
+
+class Module(nn.Module):
+    def __init__(
+        self,
+        n_users: int,
+        n_items: int,
+        n_factors: int,
+        hidden: list,
+        dropout: float,
+        interactions: torch.Tensor, 
+    ):
+        super(Module, self).__init__()
+        # attr dictionary for load
+        self.init_args = locals().copy()
+        del self.init_args["self"]
+        del self.init_args["__class__"]
+
+        # device setting
+        DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
+        self.device = torch.device(DEVICE)
+
+        # global attr
+        self.n_users = n_users
+        self.n_items = n_items
+        self.n_factors = n_factors
+        self.hidden = hidden
+        self.dropout = dropout
+        self.interactions = interactions.to(self.device)
+
+        # debugging args error
+        self._assert_arg_error()
+
+        # generate layers
+        self._init_layers()
+
+    def forward(
+        self, 
+        user_idx: torch.Tensor, 
+        item_idx: torch.Tensor,
+    ):
+        """
+        user_idx: (B,)
+        item_idx: (B,)
+        """
+        return self.score(user_idx, item_idx)
+
+    def predict(
+        self, 
+        user_idx: torch.Tensor, 
+        item_idx: torch.Tensor,
+    ):
+        """
+        user_idx: (B,)
+        item_idx: (B,)
+        """
+        with torch.no_grad():
+            logit = self.score(user_idx, item_idx)
+            pred = torch.sigmoid(logit)
+        return pred
+
+    def score(self, user_idx, item_idx):
+        pred_vector = self.ncf(user_idx, item_idx)
+        logit = self.logit_layer(pred_vector).squeeze(-1)
+        return logit
+
+    def ncf(self, user_idx, item_idx):
+        rep_user, rep_item = self.rep(user_idx, item_idx)
+        
+        concat = torch.cat(
+            tensors=(rep_user, rep_item), 
+            dim=-1,
+        )
+
+        pred_vector = self.mlp_layers(concat)
+
+        return pred_vector
+
+    def rep(self, user_idx, item_idx):
+        user_id = self.user_id_embed(user_idx)
+        user_hist = self.user_hist_embed(user_idx, item_idx)
+        rep_user = torch.cat(
+            tensors=(user_id, user_hist), 
+            dim=-1,
+        )
+
+        item_id = self.item_id_embed(item_idx)
+        item_hist = self.item_hist_embed(user_idx, item_idx)
+        rep_item = torch.cat(
+            tensors=(item_id, item_hist), 
+            dim=-1,
+        )
+
+        return rep_user, rep_item
+
+    def user_hist_embed(self, user_idx, item_idx):
+        # get user vector from interactions
+        user_slice = self.interactions[user_idx, :-1].clone()
+        
+        # masking target items
+        user_batch = torch.arange(user_idx.size(0))
+        user_slice[user_batch, item_idx] = 0
+        
+        # projection
+        proj_user = self.proj_u(user_slice.float())
+
+        return proj_user
+
+    def item_hist_embed(self, user_idx, item_idx):
+        # get item vector from interactions
+        item_slice = self.interactions.T[item_idx, :-1].clone()
+        
+        # masking target users
+        item_batch = torch.arange(item_idx.size(0))
+        item_slice[item_batch, user_idx] = 0
+        
+        # projection
+        proj_item = self.proj_i(item_slice.float())
+
+        return proj_item
+
+    def _init_layers(self):
+        self.user_id_embed = nn.Embedding(
+            num_embeddings=self.n_users+1, 
+            embedding_dim=self.n_factors,
+            padding_idx=self.n_users,
+        )
+        self.item_id_embed = nn.Embedding(
+            num_embeddings=self.n_items+1, 
+            embedding_dim=self.n_factors,
+            padding_idx=self.n_items,
+        )
+        self.proj_u = nn.Linear(
+            in_features=self.n_items,
+            out_features=self.n_factors,
+            bias=False,
+        )
+        self.proj_i = nn.Linear(
+            in_features=self.n_users,
+            out_features=self.n_factors,
+            bias=False,
+        )
+        self.mlp_layers = nn.Sequential(
+            *list(self._generate_layers(self.hidden))
+        )
+        self.logit_layer = nn.Linear(
+            in_features=self.hidden[-1],
+            out_features=1,
+        )
+
+    def _generate_layers(self, hidden):
+        idx = 1
+        while idx < len(hidden):
+            yield nn.Linear(hidden[idx-1], hidden[idx])
+            yield nn.LayerNorm(hidden[idx])
+            yield nn.ReLU()
+            yield nn.Dropout(self.dropout)
+            idx += 1
+
+    def _assert_arg_error(self):
+        CONDITION = (self.hidden[0] == self.n_factors * 4)
+        ERROR_MESSAGE = f"First MLP layer must match input size: {self.n_factors * 4}"
+        assert CONDITION, ERROR_MESSAGE
